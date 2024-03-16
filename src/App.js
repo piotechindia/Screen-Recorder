12f14421235cfbb69602cbac140bd4f87886df25
Gaurav Sharma <104793185+gauravsharma8155@users.noreply.github.com>
2024-03-16 04:50:57 +0000

    mobile implement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Landing from "./Components/Landing";
 import Screen from "./Components/Screen";
 import Camera from './Components/Camera'
 import ScreenCam from './Components/ScreenCam'
 import Pratice from "./Components/Pratice"
 function App() {
+  function isMobileBrowser() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  // Example usage
+  if (isMobileBrowser()) {
+    return <h1>This Service Only in Browser </h1>
+  }
+  isMobileBrowser();
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Landing/>}/>
-        <Route  element={<Screen/>} path="/screen"/>
-        <Route  element={<Camera/>} path="/Camera"/>
-        <Route  element={<ScreenCam/>} path="/ScreenCam"/>
-        <Route  element={<Pratice/>} path="/Pratice"/>
+        <Route path="/" element={<Landing />} />
+        <Route element={<Screen />} path="/screen" />
+        <Route element={<Camera />} path="/Camera" />
+        <Route element={<ScreenCam />} path="/ScreenCam" />
+        <Route element={<Pratice />} path="/Pratice" />
 
       </Routes>
     </div>
